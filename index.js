e5673e8be395d24954d549d28b3d49c52561c023
Oliver <oliver.wu.1987@gmail.com>
2021-11-22 10:16:19 +0000

    v6

--- index.js
+++ index.js
@@ -1,53 +1,114 @@
 class GloBar {
     constructor(initOptions = {}) {
         const defaultOptions = {
-            bandwidthList: [],
             dom: null,
+            barConfig: [],
         };
         const options = {...defaultOptions, ...initOptions};
         this.template = 
         `<div class="globarWrapper">
             <datalist class="labels">
             </datalist>
-            <input type="range" list="labels" class="bar" min="0" max="5000" step="1" value="0" >
+            <input type="range" list="labels" class="bar" min="0" max="5000" step="1" value="0" />
+            <div class="segments">
+            </div>
         </div>`;
+        this.value = null;
         this.bar = null;
-        this.bandwidthList = options.bandwidthList;
+        this.barConfig = options.barConfig;
+        this.bandwidthList = [];
+        options.barConfig.forEach(item => {
+            this.bandwidthList = this.bandwidthList.concat(item.group);
+        });
+        const validBandwithCount = this.bandwidthList.reduce((prev, curr) => {
+            if(curr.unselectable) {
+                return prev;
+            } else {
+                return prev + 1;
+            }
+        }, 0);
         this.dom = options.dom;
-        if(this.dom && this.bandwidthList.length > 0 ) {
+        if(this.dom && this.bandwidthList.length > 0 && validBandwithCount > 0) {
             this.init(options);
         }
     }
   
-    drawBar (options) {
+    drawBar () {
         this.dom.insertAdjacentHTML('beforeEnd', this.template);
         this.bar = this.dom.querySelector('.bar');
-        this.bar.value = parseInt(parseInt(this.bar.getAttribute('max')) / this.bandwidthList.length);
+        for (let i = 0; i < this.bandwidthList.length; i ++) {
+            if(this.bandwidthList[i].default) {
+                this.bar.value = parseInt(parseInt(this.bar.getAttribute('max')) * (i + 1) / this.bandwidthList.length);
+                break;
+            }
+        }
     }
 
     drawLabels () {
         const labels = this.dom.querySelector('.labels');
-        let options = `<option value="0" label="0"></option>`;
-        this.bandwidthList.forEach((item, index) => {
-            options += `<option value="${item.value}" label="${item.label}"></option>`;
+        let optionsHtml = `<option value="0" label="0"></option>`;
+        this.bandwidthList.forEach((item) => {
+            optionsHtml += `<option value="${item.value}" label="${item.label}"></option>`;
         });
-        labels.insertAdjacentHTML('beforeEnd', options);
+        labels.insertAdjacentHTML('beforeEnd', optionsHtml);
+    }
+
+    drawColors () {
+        let bg = this.barConfig[0].color + ' 0%,';
+        let length = 0;
+        for (let i = 0; i < this.barConfig.length; i ++) {
+            length += this.barConfig[i].group.length;
+            bg += this.barConfig[i].color + ' ' + Number.parseFloat(length / this.bandwidthList.length).toPrecision(10) * 100 + '%,';
+            if (i != this.barConfig.length - 1) {
+                bg += this.barConfig[i + 1].color + ' ' + Number.parseFloat(length / this.bandwidthList.length).toPrecision(10) * 100 + '%,';
+            }
+        }
+        bg = 'linear-gradient(90deg, ' + bg.slice(0, -1) + ')';
+        this.bar.style.backgroundImage = bg;
+    }
+
+    drawSegments () {
+        const segments = this.dom.querySelector('.segments');
+        this.barConfig.forEach((item) => {
+            const span = document.createElement('span');
+            span.innerText = item.text;
+            const i = document.createElement('i');
+            i.style.backgroundColor = item.color;
+            span.insertBefore(i, span.firstChild);
+            segments.appendChild(span);
+        }); 
+    }
+
+    setValue(value) {
+        this.value = value;
+    }
+
+    getValue() {
+        return this.value;
     }
 
     adjustPinPosition (currentPosition) {
         const sections = this.bandwidthList.length;
-        let diffTmp = -1;
-        let posIndex = sections;
-        for (let i = 1; i <= sections; i ++) {
-            const sectionPos = i / sections;
-            const diff = Math.abs(currentPosition - sectionPos);
-            if (diff > diffTmp && diffTmp !== -1) {
-                posIndex = i - 1;
-                break;
+        const diffs = this.bandwidthList.map((item, index) => {
+            if (item.unselectable) {
+                return Infinity;
+            } else {
+                return Math.abs(currentPosition - ((index + 1) / sections));
             }
-            diffTmp = diff;
-        }
-        return Number.parseFloat(posIndex / sections).toPrecision(10);
+        });
+        let pinIndex = -1;
+        diffs.reduce((prev, curr, index) => {
+            if (curr < prev) {
+                pinIndex = index;
+                return curr;
+            } else {
+                return prev;
+            }
+        }, Infinity);
+
+        this.setValue(this.bandwidthList[pinIndex].value);
+
+        return Number.parseFloat((pinIndex + 1) / sections).toPrecision(10);
     }
 
     movePin (pinPosition) {
@@ -60,41 +121,51 @@ class GloBar {
             this.movePin(this.adjustPinPosition(currentPosition));
         });
     }
+
     init (options) {
         this.drawBar(options);
         this.drawLabels();
+        this.drawColors();
+        this.drawSegments();
         this.handleChange();
     }
 }
 const gloBar = new GloBar({
     dom: document.getElementById('globar'),
-    bandwidthList: [{
-        label: '10M', 
-        value: 10,
-        color: 'G'
+    barConfig: [{
+        color: 'rgba(35,115,106,1)',
+        group: [{
+            label: '20MB',
+            value: '20',
+        }, {
+            label: '100MB',
+            value: '100',
+            default: true,
+        }, {
+            label: '200MB',
+            value: '200',
+        }, {
+            label: '250MB',
+            value: '250',
+            unselectable: true,
+        }],
+        text: 'G1 Device On-Net',
     }, {
-        label: '50M',
-        value: 50,
-        color: 'G'
+        color: 'rgba(249,211,113,1)',
+        group: [{
+            label: '500MB',
+            value: '500',
+        }, {
+            label: '1GE',
+            value: '1000',
+        }],
+        text: 'Amber Fibre Near-Net',
     }, {
-        label: '100M',
-        value: 100,
-        color: 'G'
-    }, {
-        label: '200M',
-        value: 200,
-        color: 'A'
-    }, {
-        label: '500M',
-        value: 500,
-        color: 'A'
-    }, {
-        label: '1GB',
-        value: 1000,
-        color: 'R'
-    }, {
-        label: '2GB',
-        value: 2000,
-        color: 'R'
+        color: 'rgba(207,54,72,1)',
+        group: [{
+            label: '2GE',
+            value: '2000',
+        }],
+        text: 'Red Unable to determine',
     }],
 });
